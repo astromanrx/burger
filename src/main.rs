06ac06350de2138e01ab6bc9b14468529af5e96d
astromanrx <astromanrx@gmail.com>
2024-09-09 04:24:36 +0330

    initial commit.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,289 @@
-fn main() {
-    println!("Hello, world!");
+use ethers::{
+    providers::{Middleware, Provider, Ws},
+    types::*,
+};
+use std::sync::Arc;
+use tokio::sync::broadcast::Sender;
+use tokio_stream::StreamExt;
+
+use crate::common::utils::calculate_next_block_base_fee;
+
+use bounded_vec_deque::BoundedVecDeque;
+use ethers::signers::{LocalWallet, Signer};
+use ethers::{
+    providers::{Middleware, Provider, Ws},
+    types::{BlockNumber, H160, H256, U256, U64},
+};
+use log::{info, warn};
+use std::{collections::HashMap, str::FromStr, sync::Arc};
+use tokio::sync::broadcast::Sender;
+
+// we'll update this part later, for now just import the necessary components
+use crate::common::constants::{Env, WETH};
+use crate::common::streams::{Event, NewBlock};
+use crate::common::utils::{calculate_next_block_base_fee, to_h160};
+use anyhow::Result;
+use ethers::core::rand::thread_rng;
+use ethers::prelude::*;
+use ethers::{
+    self,
+    types::{
+        transaction::eip2930::{AccessList, AccessListItem},
+        U256,
+    },
+};
+use fern::colors::{Color, ColoredLevelConfig};
+use foundry_evm_mini::evm::utils::{b160_to_h160, h160_to_b160, ru256_to_u256, u256_to_ru256};
+use log::LevelFilter;
+use rand::Rng;
+use revm::primitives::{B160, U256 as rU256};
+use std::str::FromStr;
+use std::sync::Arc;
+
+use crate::common::constants::{PROJECT_NAME, WETH};
+
+// Function to format our console logs
+pub fn setup_logger() -> Result<()> {
+    let colors = ColoredLevelConfig {
+        trace: Color::Cyan,
+        debug: Color::Magenta,
+        info: Color::Green,
+        warn: Color::Red,
+        error: Color::BrightRed,
+        ..ColoredLevelConfig::new()
+    };
+
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{}[{}] {}",
+                chrono::Local::now().format("[%H:%M:%S]"),
+                colors.color(record.level()),
+                message
+            ))
+        })
+        .chain(std::io::stdout())
+        .level(log::LevelFilter::Error)
+        .level_for(PROJECT_NAME, LevelFilter::Info)
+        .apply()?;
+
+    Ok(())
+}
+
+// Calculates the next block base fee given the previous block's gas usage / limits
+// Refer to: https://www.blocknative.com/blog/eip-1559-fees
+pub fn calculate_next_block_base_fee(
+    gas_used: U256,
+    gas_limit: U256,
+    base_fee_per_gas: U256,
+) -> U256 {
+    let gas_used = gas_used;
+
+    let mut target_gas_used = gas_limit / 2;
+    target_gas_used = if target_gas_used == U256::zero() {
+        U256::one()
+    } else {
+        target_gas_used
+    };
+
+    let new_base_fee = {
+        if gas_used > target_gas_used {
+            base_fee_per_gas
+                + ((base_fee_per_gas * (gas_used - target_gas_used)) / target_gas_used)
+                    / U256::from(8u64)
+        } else {
+            base_fee_per_gas
+                - ((base_fee_per_gas * (target_gas_used - gas_used)) / target_gas_used)
+                    / U256::from(8u64)
+        }
+    };
+
+    let seed = rand::thread_rng().gen_range(0..9);
+    new_base_fee + seed
+}
+
+pub fn access_list_to_ethers(access_list: Vec<(B160, Vec<rU256>)>) -> AccessList {
+    AccessList::from(
+        access_list
+            .into_iter()
+            .map(|(address, slots)| AccessListItem {
+                address: b160_to_h160(address),
+                storage_keys: slots
+                    .into_iter()
+                    .map(|y| H256::from_uint(&ru256_to_u256(y)))
+                    .collect(),
+            })
+            .collect::<Vec<AccessListItem>>(),
+    )
+}
+
+pub fn access_list_to_revm(access_list: AccessList) -> Vec<(B160, Vec<rU256>)> {
+    access_list
+        .0
+        .into_iter()
+        .map(|x| {
+            (
+                h160_to_b160(x.address),
+                x.storage_keys
+                    .into_iter()
+                    .map(|y| u256_to_ru256(y.0.into()))
+                    .collect(),
+            )
+        })
+        .collect()
+}
+
+abigen!(
+    IERC20,
+    r#"[
+        function balanceOf(address) external view returns (uint256)
+    ]"#,
+);
+
+// Utility functions
+
+pub async fn get_token_balance(
+    provider: Arc<Provider<Ws>>,
+    owner: H160,
+    token: H160,
+) -> Result<U256> {
+    let contract = IERC20::new(token, provider);
+    let token_balance = contract.balance_of(owner).call().await?;
+    Ok(token_balance)
+}
+
+pub fn create_new_wallet() -> (LocalWallet, H160) {
+    let wallet = LocalWallet::new(&mut thread_rng());
+    let address = wallet.address();
+    (wallet, address)
+}
+
+pub fn to_h160(str_address: &'static str) -> H160 {
+    H160::from_str(str_address).unwrap()
+}
+
+pub fn is_weth(token_address: H160) -> bool {
+    token_address == to_h160(WETH)
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct NewBlock {
+    pub block_number: U64,
+    pub base_fee: U256,
+    pub next_base_fee: U256,
+}
+
+#[derive(Debug, Clone)]
+pub struct NewPendingTx {
+    pub added_block: Option<U64>,
+    pub tx: Transaction,
+}
+
+impl Default for NewPendingTx {
+    fn default() -> Self {
+        Self {
+            added_block: None,
+            tx: Transaction::default(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Event {
+    Block(NewBlock),
+    PendingTx(NewPendingTx),
+}
+
+// A websocket connection made to get newly created blocks
+pub async fn stream_new_blocks(provider: Arc<Provider<Ws>>, event_sender: Sender<Event>) {
+    let stream = provider.subscribe_blocks().await.unwrap();
+    let mut stream = stream.filter_map(|block| match block.number {
+        Some(number) => Some(NewBlock {
+            block_number: number,
+            base_fee: block.base_fee_per_gas.unwrap_or_default(),
+            next_base_fee: U256::from(calculate_next_block_base_fee(
+                block.gas_used,
+                block.gas_limit,
+                block.base_fee_per_gas.unwrap_or_default(),
+            )),
+        }),
+        None => None,
+    });
+
+    while let Some(block) = stream.next().await {
+        match event_sender.send(Event::Block(block)) {
+            Ok(_) => {}
+            Err(_) => {}
+        }
+    }
+}
+
+// A websocket connection made to get new pending transactions
+pub async fn stream_pending_transactions(provider: Arc<Provider<Ws>>, event_sender: Sender<Event>) {
+    let stream = provider.subscribe_pending_txs().await.unwrap();
+    let mut stream = stream.transactions_unordered(256).fuse();
+
+    while let Some(result) = stream.next().await {
+        match result {
+            Ok(tx) => match event_sender.send(Event::PendingTx(NewPendingTx {
+                added_block: None,
+                tx,
+            })) {
+                Ok(_) => {}
+                Err(_) => {}
+            },
+            Err(_) => {}
+        };
+    }
+}
+
+pub async fn run_sandwich_strategy(provider: Arc<Provider<Ws>>, event_sender: Sender<Event>) {
+    let mut event_receiver = event_sender.subscribe();
+
+    loop {
+        match event_receiver.recv().await {
+            Ok(event) => match event {
+                Event::Block(block) => {
+                    info!("{:?}", block);
+                }
+                Event::PendingTx(mut pending_tx) => {
+                    info!("{:?}", pending_tx);
+                }
+            },
+            _ => {}
+        }
+    }
+}
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+    setup_logger().unwrap();
+
+    let env = Env::new();
+
+    let ws = Ws::connect(env.wss_url.clone()).await.unwrap();
+    let provider = Arc::new(Provider::new(ws));
+
+    let (event_sender, _): (Sender<Event>, _) = broadcast::channel(512);
+
+    let mut set = JoinSet::new();
+
+    set.spawn(stream_new_blocks(provider.clone(), event_sender.clone()));
+    set.spawn(stream_pending_transactions(
+        provider.clone(),
+        event_sender.clone(),
+    ));
+
+    set.spawn(run_sandwich_strategy(
+        provider.clone(),
+        event_sender.clone(),
+    ));
+
+    while let Some(res) = set.join_next().await {
+        info!("{:?}", res);
+    }
+
+    Ok(())
 }
